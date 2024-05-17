3f3befb24dda3fdc61ed0e5d5a074f628da95757
zhazorken <zhazorken@gmail.com>
2024-05-16 17:57:29 -0700

    added pbs submission script

---
+++ pbs_iceplume.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -l
+#PBS -A UCLA0052
+#PBS -N refreflo
+#PBS -k eod
+#PBS -o logs/DNSkerr.out
+#PBS -e logs/DNSkerr.err
+#PBS -l walltime=12:00:00
+#PBS -q casper
+#PBS -l select=1:ncpus=1:ngpus=1
+#PBS -l gpu_type=v100
+#PBS -M zhazorken@gmail.com
+#PBS -m abe
+
+# Clear the environment from any previously loaded modules
+module purge
+module load ncarenv/23.10
+module load julia/1.9.2 cuda
+
+module load peak-memusage
+module li
+
+#/glade/u/apps/ch/opt/usr/bin/dumpenv # Dumps environment (for debugging with CISL support)
+
+#export JULIA_DEPOT_PATH="/glade/work/tomasc/.julia_bkp"
+export JULIA_DEPOT_PATH="/glade/work/kenzhao/.julia"
+
+peak_memusage julia --project iceplume.jl --simname=DNSgayen3casper --factor=2 2>&1 | tee out/DNSgayen1.out
